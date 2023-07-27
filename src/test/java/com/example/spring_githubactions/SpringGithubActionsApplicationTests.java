52ac10f6e370c74df65470e65cab99cfdea9bde3
k2taee <rlxo1992@gmail.com>
2023-07-27 10:05:10 +0900

    first commit

---
+++ src/test/java/com/example/spring_githubactions/SpringGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_githubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGithubActionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
